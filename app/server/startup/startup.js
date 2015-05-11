09bad819c261a56866e82721594673aecb24ad5a
MattReisman <mattreisman@gmail.com>
2015-05-11 16:43:23 -0600

    Simplify folder organization

--- app/server/startup/startup.js
+++
@@ -1,7 +0,0 @@
-// ***************************************************************
-// STARTUP (Will run as soon as the server process is finished starting)
-// ***************************************************************
-
-Meteor.startup(function () {
-  // Do something
-});
