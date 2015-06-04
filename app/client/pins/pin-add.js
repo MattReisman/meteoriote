af258c6e328b6a8acea5fd4c4778eb450f4cb27a
MattReisman <mattreisman@gmail.com>
2015-06-04 13:37:49 -0600

    fix pin add route

--- app/client/pins/pin-add.js
+++ app/client/pins/pin-add.js
@@ -1,8 +1,9 @@
 Template.pinAdd.events({
   'submit form': function(e) {
     e.preventDefault();
-    var deviceId = Session.get('deviceID');
-    Router.go('dashboard.device', {'_id': 'deviceId'});
+    var deviceId = Devices.findOne();
+    var thisId = this._id;
+    Router.go('dashboard.device');
   }
 });
 
